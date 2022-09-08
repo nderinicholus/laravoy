a097cae8f0465617a03167f21006f4325b8b7a38
nderinicholus <mwaniki.nderi@gmail.com>
2022-09-09 02:27:06 +0300

    Setting up relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'author_id', 'category_id', 'title', 'seo_title', 'excerpt', 'body', 'image', 'slug', 'meta_description', 'meta_keywords', 'status', 'featured', 'user_id'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+ 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    protected static function booted()
+    {
+        if(auth()->check()) {
+            static::addGlobalScope('by_user', function (Builder $builder) {
+                $builder->where('user_id', '=', auth()->id());
+            });
+        }
+    }
 }
